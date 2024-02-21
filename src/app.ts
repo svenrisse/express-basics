43c1e43f0d12bf96408a1973dd4fc509cf215148
Sven <svenrisse0@gmail.com>
2024-02-21 19:00:31 +0100

    feat: finalize tutorial

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
-import express from "express";
+import express, {
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
 
 const app = express();
 
@@ -14,8 +18,41 @@ app
   })
   .put((req, res) => {
     return res.send("You made a PUT request");
+  })
+  .delete((req, res) => {
+    return res.send("You made a DELETE request");
   });
 
-app.listen(3000, () => {
-  console.log("app listening at port 3000");
+const middleware =
+  (name: string) => (req: Request, res: Response, next: NextFunction) => {
+    res.locals.name = name;
+    next();
+  };
+
+app.use(middleware("sven"));
+
+const handleGetBook = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.params);
+
+  console.log(res.locals.name);
+  return res.send(req.params);
+};
+
+app.get("/api/books/:bookId", handleGetBook);
+
+const throwsError = async () => {
+  throw new Error("BoomB");
+};
+
+app.get("/error", async (req, res) => {
+  try {
+    await throwsError();
+    res.status(200);
+  } catch {
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.listen(3001, () => {
+  console.log("app listening at port 3001");
 });
